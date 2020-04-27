2157c8f3d923c1f38659491c5d768063253a5fbf
Ian McKenzie <imckenzie777@gmail.com>
2020-04-27 20:34:35 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -4,10 +4,3 @@ def loop_message_five_times(s)
   end
 end
 loop_message_five_times("Flatiron School is Awesome")
-
-def loop_message_five_times(n.length)
-  5.times do
-  	puts n
-  end
-end
-loop_message_five_times("Flatiron")
