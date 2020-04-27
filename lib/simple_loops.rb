5f73c730ded5b02cb009d8bb8d1ae909479728f1
Ian McKenzie <imckenzie777@gmail.com>
2020-04-27 20:24:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,7 @@
 def loop_message_five_times(array)
   count = 0
-
-  for count < array.length do
-    puts array[counter]
+  #
+  # for count < array.length do
+  #   puts array[counter]
   end
+10.times do { puts array }
